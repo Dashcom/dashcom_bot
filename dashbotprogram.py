577cfdbc32959f6805d4f1d45b13c0e21f71e4c2
Dashcom <baltazar@iMax.local>
2015-06-10 23:48:38 +0200

    update

--- dashbotprogram.py
+++ dashbotprogram.py
@@ -5,8 +5,8 @@ import tweepy, time, sys
 #argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = ' IicoNuhSOlY6ciZ9bdzzbva8s'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = ' Cmr35qwqd1n06tdDtYmRzbqmYvZFXQmrLmsWDenKf3GYZTFBXO'#keep the quotes, replace this with your consumer secret key
+CONSUMER_KEY = 'IicoNuhSOlY6ciZ9bdzzbva8s'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'Cmr35qwqd1n06tdDtYmRzbqmYvZFXQmrLmsWDenKf3GYZTFBXO'#keep the quotes, replace this with your consumer secret key
 ACCESS_KEY = '3317742490-VCmb9dUCXxtiKjOVBigRfDWQEko34TOJmRqZB9W'#keep the quotes, replace this with your access token
 ACCESS_SECRET = 'OIunphd5UoKe8kMFAAGoHnOuJ5GXF2ROceE39feBlZ8eS'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
