ee9b526af44f7dd06e4e91d68acf2cad6f4d98eb
Dashcom <bcdm17@live.fr>
2015-06-20 00:45:07 +0200

    Résolution des conflits de commit

--- Création/random.py
+++ Création/random.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: UTF-8 -*-
 import numpy
 from numpy import random
 dico=open('../Résolution/dico8.txt', 'r')
@@ -6,30 +6,45 @@ list=(dico.readline()).split()
 #print(list)
 wd=list[random.randint(0,len(list))]
 letter=0
+tr=1
 #nécessité de modifier les print en envoi de tweet
 print('mot de ', len(wd), 'lettres', 'commencant par', wd[0])
 verif=[]
-print(wd)
+#print(wd)
 usr=input('entrer un mot ')
-if wd in list == True:
+pos=[]
+if wd in list:
     for i in range(0, len(wd)):
-        if wd[i]==usr[i]:
-            verif.append(2)
+        if usr[i] in wd:
+            if wd[i]==usr[i]:
+                verif.append(2)
+            else:
+                verif.append(1)
         else:
             verif.append(0)
     print(verif)
 else:
     print('\'\'')
+#if usr=='SOLUCE0401':
+#    print(wd)
 while usr!=wd:
     verif=[]
     usr=input('entrer un mot ')
-    if wd in list == True:
+    if wd in list:
+        pos=wd
         for i in range(0, len(wd)):
-            if wd[i]==usr[i]:
-                verif.append(2)
+            if usr[i] in wd:
+                if wd[i]==usr[i]:
+                    verif.append(2)
+                    pos.remove(i)
+                else:
+                    verif.append(1)
             else:
                 verif.append(0)
         print(verif)
     else:
         print('\'\'')
-print('c\'est bien', usr, 'bravo')
+#    if usr=='SOLUCE0401':
+        print(wd)
+#    tr=(tr+1)
+print('c\'est bien', usr, 'trouve en', tr, 'essais.')
