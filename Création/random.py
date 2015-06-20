6c2f7251d44948b609da691cc678e60877a49789
Dashcom <baltazar@MacBook.local>
2015-06-20 20:02:53 +0200

    Fixing issue "Ramdom.py : wrong number of lettres with verification '1' #4"

--- Création/random.py
+++ Création/random.py
@@ -3,44 +3,62 @@ import numpy
 from numpy import random
 dico=open('../Résolution/Dico_final.txt', 'r')
 list=(dico.readline()).split()
-#print(list)
 wd=list[random.randint(0,len(list))]
 letter=0
 tr=1
 #nécessité de modifier les print en envoi de tweet
 print('Mot de {} lettres, commencant par {}'.format(len(wd), wd[0]))
 verif=[]
-#print(wd)
 usr=raw_input('entrer un mot : ')
 pos=[]
+usr1=[]
+if usr=='DEFINE':
+    wd=raw_input('wd ?')
+    usr=raw_input('entrer un mot : ')
 if wd in list:
     for i in range(0, len(wd)):
-        if usr[i] in wd:
-            if wd[i]==usr[i]:
-                verif.append(2)
-            else:
-                verif.append(1)
+        pos.append(wd[i])
+        usr1.append(usr[i])
+    for i in range(0, len(wd)):
+        if usr[i]==wd[i]:
+            verif.append(2)
+            pos[i]=0
+            usr1[i]=2
         else:
             verif.append(0)
+    for i in range(0, len(pos)):
+        if usr1[i] in pos:
+            verif[i]=1
+            rm=pos.index(usr1[i])
+            pos[rm]=0
     print(verif)
 else:
     print('\'\'')
 if usr=='SOLUCE0401':
     print(wd)
+if usr=='DEFINE':
+    wd=raw_input('wd ?')
 while usr!=wd:
     verif=[]
+    pos=[]
+    usr1=[]
     usr=raw_input('entrer un mot : ')
     if wd in list:
-        pos=wd
         for i in range(0, len(wd)):
-            if usr[i] in wd:
-                if wd[i]==usr[i]:
-                    verif.append(2)
-                    #pos.remove(i)
-                else:
-                    verif.append(1)
+            pos.append(wd[i])
+            usr1.append(usr[i])
+        for i in range(0, len(wd)):
+            if wd[i]==usr[i]:
+                verif.append(2)
+                pos[i]=0
+                usr1[i]=2
             else:
                 verif.append(0)
+        for i in range(0, len(pos)):
+            if usr1[i] in pos:
+                verif[i]=1
+                rm=pos.index(usr1[i])
+                pos[rm]=1
         print(verif)
     else:
         print('\'\'')
