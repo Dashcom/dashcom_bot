a385ff61767a60ccb405570b092457fb8c04b494
Shurtugal <mathias.michel99@orange.fr>
2015-06-20 17:40:32 +0200

    More efficient (includes the need of a lettre if verification '1') + imrovement of inputs efficiency

--- Résolution/Reso.py
+++ Résolution/Reso.py
@@ -1,12 +1,16 @@
 liste=[]
 nope=[]
-L=input("Premiere lettre : ")
+l12={}
+L=raw_input("Premiere lettre : ")
 N=input("Nombre de lettres : ")
 dico=open('Dico8.txt', 'r')
 dico=(dico.readline()).split()
 for x in range(0, len(dico)):
 	if dico[x].startswith(L) and len(dico[x])==N:
 		liste.append(dico[x].lower())
+a='azertyuiopqsdfghjklmwxcvbn'
+for b in range(0,26):
+	l12[a[b]]=0
 while len(liste)>1:
 	sl1={}
 	l0=[]
@@ -15,6 +19,7 @@ while len(liste)>1:
 	a='azertyuiopqsdfghjklmwxcvbn'
 	for b in range(0,26):
 		sl1[a[b]]=0
+	l12p=l12.copy()
 	l12=sl1.copy()
 	for y in range(0,N):
 		sl2=sl1.copy()
@@ -30,7 +35,7 @@ while len(liste)>1:
 		if S[x]>S[M]:
 			M=x
 	print(liste[M])
-	R=input()
+	R=str(input())
 	if R=='':
 		nope.append(liste[M])
 		del liste[M]
@@ -50,6 +55,9 @@ while len(liste)>1:
 				l0.append(liste[M][y])
 			else:
 				l12[liste[M][y]]=l12[liste[M][y]]+1
+		for b in l12:
+			if l12p[b]>l12[b]:
+				l12[b]=l12p[b]
 		liste3=[]
 		for x in range(0,len(liste2)):
 			E=0
@@ -60,6 +68,14 @@ while len(liste)>1:
 					E=E+1
 			if E==N:
 				liste3.append(liste2[x])
-		liste=liste3[:]
+		liste4=[]
+		for x in range(0,len(liste3)):
+			E=0
+			for b in l12:
+				if liste3[x].count(b)>=l12[b]:
+					E=E+1
+			if E==26:
+				liste4.append(liste3[x])
+		liste=liste4[:]
 print(liste[0])
 print(nope)
