e798d88140c65b575044203640812842cc0c5519
Dashcom <baltazar@MacBook.local>
2015-06-22 15:07:24 +0200

    Essai de programme bot

--- Bot/dashbotprogram.py
+++ Bot/dashbotprogram.py
@@ -1,3 +1,76 @@
 # -*- coding: utf-8 -*-
-from twitter import *
-t = Twitter(auth=OAuth(#KEY HERE))
+import twitter
+t = twitter.Twitter(auth=OAuth('3317742490-VCmb9dUCXxtiKjOVBigRfDWQEko34TOJmRqZB9W', 'OIunphd5UoKe8kMFAAGoHnOuJ5GXF2ROceE39feBlZ8eS','IicoNuhSOlY6ciZ9bdzzbva8s', 'Cmr35qwqd1n06tdDtYmRzbqmYvZFXQmrLmsWDenKf3GYZTFBXO'))
+import numpy
+from numpy import random
+dico=open('../Résolution/Dico_final.txt', 'r')
+list=(dico.readline()).split()
+wd=list[random.randint(0,len(list))]
+letter=0
+tr=1
+#nécessité de modifier les print en envoi de tweet
+print('Mot de {} lettres, commencant par {}'.format(len(wd), wd[0]))
+t.statuses.update(status='Mot de {} lettres, commencant par {}'.format(len(wd), wd[0]))
+verif=[]
+usr=raw_input('entrer un mot : ')
+pos=[]
+usr1=[]
+if usr=='DEFINE':
+    wd=raw_input('wd ?')
+    usr=raw_input('entrer un mot : ')
+if usr in list:
+    for i in range(0, len(wd)):
+        pos.append(wd[i])
+        usr1.append(usr[i])
+    for i in range(0, len(wd)):
+        if usr[i]==wd[i]:
+            verif.append(2)
+            pos[i]=0
+            usr1[i]=2
+        else:
+            verif.append(0)
+    for i in range(0, len(pos)):
+        if usr1[i] in pos:
+            verif[i]=1
+            rm=pos.index(usr1[i])
+            pos[rm]=0
+    print(verif)
+    t.statuses.update(status=verif)
+else:
+    print('\'\'')
+    t.statuses.update(status='\'\'')
+if usr=='SOLUCE0401':
+    print(wd)
+if usr=='DEFINE':
+    wd=raw_input('wd ?')
+while usr!=wd:
+    verif=[]
+    pos=[]
+    usr1=[]
+    usr=raw_input('entrer un mot : ')
+    if usr in list:
+        for i in range(0, len(wd)):
+            pos.append(wd[i])
+            usr1.append(usr[i])
+        for i in range(0, len(wd)):
+            if wd[i]==usr[i]:
+                verif.append(2)
+                pos[i]=0
+                usr1[i]=2
+            else:
+                verif.append(0)
+        for i in range(0, len(pos)):
+            if usr1[i] in pos:
+                verif[i]=1
+                rm=pos.index(usr1[i])
+                pos[rm]=1
+        print(verif)
+        t.twitter.statuses.update(status=verif)
+    else:
+        print('\'\'')
+        t.twitter.statuses.update(status='\'\'')
+    if usr=='SOLUCE0401':
+        print(wd)
+    tr=(tr+1)
+print('C\'est bien {}, trouve en {} essais.'.format(usr, tr))
+t.twitter.statuses.update(status='C\'est bien {}, trouve en {} essais.'.format(usr, tr))
